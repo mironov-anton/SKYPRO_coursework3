04915f6d8067afa0ae160deb8b7160774a0dfd45
mironov-anton <miron.khuzd@gmail.com>
2022-02-12 23:08:09 +0300

    Models and schemas ready, fixtures data loaded to db

---
+++ project/dao/models/user.py
@@ -0,0 +1,14 @@
+from project.dao.models.base import BaseMixin
+from project.setup_db import db
+
+
+class User(BaseMixin, db.Model):
+    __tablename__ = "user"
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255))
+    surname = db.Column(db.String(255))
+    favorite_genre = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f"<User '{self.email}'>"
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from project.config import DevelopmentConfig
-from project.dao.models import Genre
+from project.dao.models import Genre, Director, Movie, User
 from project.server import create_app, db
 
 app = create_app(DevelopmentConfig)
@@ -10,4 +10,7 @@ def shell():
     return {
         "db": db,
         "Genre": Genre,
+        "Director": Director,
+        "Movie": Movie,
+        "User": User
     }
